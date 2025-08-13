a9582abf5bf63dcaec0c2ffd78392b2976e6508d
Wilbis Paulo <wilbispaulo@hotmail.com>
2025-08-13 12:31:19 -0300

    .

--- app/routes/web.php
+++ app/routes/web.php
@@ -17,16 +17,16 @@ $app->get('/api/pix/txt', [TesteController::class, 'index'])
 $app->get('/api/teste/one/{id}', [TesteController::class, 'teste']);
 $app->post('/api/bingo/create', [BingoController::class, 'create'])
     ->add(new JsonBingoCartValidatorMiddleware())
-    ->add(new JsonBodyParserMiddleware())
-    ->add(new OAuthMiddleware());
+    ->add(new JsonBodyParserMiddleware());
+    //->add(new OAuthMiddleware());
 $app->post('/api/bingo/upload/json', [UploadController::class, 'uploadJson'])
-    ->add(new JsonFileSignatureValidatorMiddleware())
-    ->add(new OAuthMiddleware());
-$app->post('/api/bingo/upload/background', [UploadController::class, 'uploadBack'])
-    ->add(new OAuthMiddleware());
+    ->add(new JsonFileSignatureValidatorMiddleware());
+    //->add(new OAuthMiddleware());
+$app->post('/api/bingo/upload/background', [UploadController::class, 'uploadBack']);
+    //->add(new OAuthMiddleware());
 $app->get('/api/bingo/pdf/generate/one_serie/two_carts/{serie}/{from}/{to}', [BingoPdfController::class, 'createOneSerieTwoCarts'])
-    ->add(new BingoFilesValidatorMiddleware())
-    ->add(new OAuthMiddleware());
+    ->add(new BingoFilesValidatorMiddleware());
+    //->add(new OAuthMiddleware());
 
 // APP
 $app->get('/app/bingo/upload', [AppController::class, 'bingoUpload']);
